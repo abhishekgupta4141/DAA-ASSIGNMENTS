9ef6f91b79728f5d97c815223b043394306c5d56
abhishekgupta4141 <princegupta4077@gmail.com>
2026-09-21 09:44:56 +0530

    Time: 24 ms (88.33%), Space: 80.8 MB (86.16%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -3,7 +3,6 @@ class Solution {
         if (nums == null || nums.length <= 1) {
             return nums;
         }
-        
         int[] temp = new int[nums.length];
         mergeSort(nums, 0, nums.length - 1, temp);
         return nums;
@@ -13,26 +12,20 @@ class Solution {
         if (left >= right) {
             return;
         }
-
         int mid = left + (right - left) / 2;
-
-       
         mergeSort(nums, left, mid, temp);
         mergeSort(nums, mid + 1, right, temp);
-
-        
         merge(nums, left, mid, right, temp);
     }
 
     private void merge(int[] nums, int left, int mid, int right, int[] temp) {
-        
         for (int i = left; i <= right; i++) {
             temp[i] = nums[i];
         }
-
-        int i = left;      
-        int j = mid + 1;   
-        int k = left;      
+        
+        int i = left;
+        int j = mid + 1;
+        int k = left;
 
         while (i <= mid && j <= right) {
             if (temp[i] <= temp[j]) {
@@ -41,9 +34,13 @@ class Solution {
                 nums[k++] = temp[j++];
             }
         }
+
         while (i <= mid) {
             nums[k++] = temp[i++];
         }
-        
+
+        while (j <= right) {
+            nums[k++] = temp[j++];
+        }
     }
 }
